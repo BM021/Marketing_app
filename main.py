557d769dd2aed52cc14e2ac156d37f3320cb4e99
muhammadamin <bakhodyrov17@inbox.ru>
2023-08-01 13:50:36 +0500

    fix error 404

--- main.py
+++ main.py
@@ -11,3 +11,8 @@ app = FastAPI()
 app.include_router(product.router)
 app.include_router(news.router)
 app.include_router(messages.router)
+
+
+@app.get('/')
+async def main():
+    return {'message': 'Hello from MARS!'}
